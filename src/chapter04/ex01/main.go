ec0609c8d7ab91e172597f3d3569053fb49c028c
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2019-07-28 19:14:45 +0900

    add

---
+++ src/chapter04/ex01/main.go
@@ -0,0 +1,36 @@
+// ２つのSHA256ハッシュで異なるビットの数を数える関数を書きなさい(2.6.2節のPopCountを参照)
+
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+var pc [8]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = byte(1 << uint(i))
+	}
+}
+
+func main() {
+	h1 := sha256.Sum256([]byte("x"))
+	h2 := sha256.Sum256([]byte("X"))
+	fmt.Println(diff(h1, h2))
+}
+
+func diff(h1, h2 [32]byte) int {
+	count := 0
+	for i := 0; i < 32; i++ {
+		b1 := h1[i]
+		b2 := h2[i]
+		for j := 0; j < 8; j++ {
+			if b1&pc[j] != b2&pc[j] {
+				count++
+			}
+		}
+	}
+	return count
+}
---
+++ src/chapter04/ex02/main.go
@@ -0,0 +1,46 @@
+// デフォルトで標準入力のSHA256ハッシュを表示するプログラムを書きなさい。
+// ただし、SHA384ハッシュやSHA512ハッシュを表示するコマンドラインのフラグもサポートしなさい。
+
+package main
+
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var width = flag.Int("w", 256, "hash width (256, 384 or 512)")
+
+func main() {
+	flag.Parse()
+
+	b, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch *width {
+	case 256:
+		h := sha256.Sum256(b)
+		print(h[:])
+	case 384:
+		h := sha512.Sum384(b)
+		print(h[:])
+	case 512:
+		h := sha512.Sum512(b)
+		print(h[:])
+	default:
+		log.Fatal("invalid hash widh specified.")
+	}
+}
+
+func print(hash []byte) {
+	for _, v := range hash {
+		fmt.Printf("%X", v)
+	}
+	fmt.Println()
+}
